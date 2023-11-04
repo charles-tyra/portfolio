1fad64d154cf47f99c0f22174be242c6b6d98438
Charles Tyra <charles.tyra@gmail.com>
2023-11-03 20:00:54 -0400

    Use of state for clicked variable.

--- src/components/Hoverable/index.js
+++ src/components/Hoverable/index.js
@@ -1,14 +1,11 @@
-// useState for boolean change?
-import React from "react";
+import React, {useState} from "react";
 import './Hoverable.css';
 
-function Hoverable(iD, img) {
-   let clicked = false;
-
-   console.log(clicked);
+function Hoverable(iD, iMG) {
+   let [clicked, setClicked] = useState(false);
 
    return (
-      <div id='testId' onClick={() => clicked = !clicked}>
+      <div id='testId' onClick={() => setClicked(!clicked)}>
          {clicked ? 
             <div>UNCLICKED DIV</div>
             : <div className="testHoverable">CLICKED DIV</div>
