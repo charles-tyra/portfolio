eb48ff3a4a69992428f64d86273dc17ab869aa8b
Charles Tyra <charles.tyra@gmail.com>
2023-11-08 00:30:56 -0500

    Working img

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import Hoverable from './components/Hoverable';
-// import background from './assets/photo12.png'
+import background from './assets/photo12.jpg'
 
 function App() {
   return (
     <div id='body-container'>
-      {/* <img id='backgroundimg' src={background} /> */}
+      <img id='backgroundimg' src={background} />
       <div id='body'>
         <Hoverable iD={'testBox'} img={'none'} />
         <Hoverable iD={'this'} img={'none'} />
