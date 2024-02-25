ba9509178cd96f6cc1431bf44b5f3758a3368ee5
Charles Tyra <charles.tyra@gmail.com>
2024-02-24 21:05:55 -0500

    More comment cleanup - back to real work tomorrow.

--- src/components/ProjectsDiv/SaharaDiv/index.js
+++ src/components/ProjectsDiv/SaharaDiv/index.js
@@ -7,11 +7,6 @@ function SaharaDiv() {
    return (
       <div id='project-container'>
          <img id='sahara-img' src={sahara} />
-         {/* <br/>
-
-         <a id='tripit-img-link' href="https://trip-it.onrender.com">
-            Trip It
-         </a> */}
       </div>
    )
 }
