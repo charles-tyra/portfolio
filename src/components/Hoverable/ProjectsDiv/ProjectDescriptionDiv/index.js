1de687b3921aaa57aa08b52a45ee98840d2cbf4c
Charles Tyra <charles.tyra@gmail.com>
2024-03-09 22:50:28 -0500

    Project Description descriptions added.

--- src/components/Hoverable/ProjectsDiv/ProjectDescriptionDiv/index.js
+++ src/components/Hoverable/ProjectsDiv/ProjectDescriptionDiv/index.js
@@ -6,6 +6,22 @@ function ProjectDescriptionDiv({projectState}) {
    return (
       <div>
          {projectState === 'TRIPIT'
+            ?  <div>
+                  MERN app where friends can collaborate to create, plan, 
+                  and make decisions about group travel
+               </div>
+            : null 
+         }
+
+         {projectState === 'SAHARA'
+            ?  <div>
+                  Fullstack Amazon clone where people can create a user account,
+                  search and review products, add them to cart and purchase them
+               </div>
+            : null 
+         }
+
+         {projectState === 'TRIPIT'
             ?  <div>test statement</div>
             : null 
          }
