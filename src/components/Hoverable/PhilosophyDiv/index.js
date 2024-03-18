6416791a3972c3e1779b9faf8bcd9cd1fbf021f8
Charles Tyra <charles.tyra@gmail.com>
2024-03-18 11:45:07 -0400

    philosophy writing update

--- src/components/Hoverable/PhilosophyDiv/index.js
+++ src/components/Hoverable/PhilosophyDiv/index.js
@@ -5,13 +5,13 @@ function PhilosophyDiv() {
 
    return (
       <div id='philosophy-inside'>
-         In school I practiced art. I enjoyed the open ended quality, the endless
-         thought that you could put into each piece. How it pushed your brain to
+         I practiced art in school. I enjoyed the open-ended quality; the endless
+         thought that you could put into each piece; the way it pushed my brain to
          not just think of one possibility, but to think of hundreds and critically
-         analyze each. Experience in the restaurant industry, with it's pace and
-         individual purpose, brought me an appreciation of real world application
-         - combining my experience and natural curiousity, to allow myself to apply
-         these creative thoughts in a more succinct approach.
+         analyze each. My experience in the restaurant industry, with its pace and
+         intentionality, brought me an appreciation of real-world application.
+         It combined my natural curiosity with practical experience, allowing me to 
+         translate creative thoughts to the functional and tangible.
          <br />
          <br />
          I hope you find my focus on detail, purpose, and thought within each of my projects.
