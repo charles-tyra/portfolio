183b660caf58616c0832590c9205929b41925c22
Charles Tyra <charles.tyra@gmail.com>
2024-03-23 20:16:08 -0400

    More cleaning in anticipation for tomorrow

--- src/components/Hoverable/ProjectsDiv/TripItDiv/index.js
+++ src/components/Hoverable/ProjectsDiv/TripItDiv/index.js
@@ -4,18 +4,21 @@ import './TripItDiv.css';
 import tripIt from '../../../../assets/tripItSplashPage.png';
 
 function TripItDiv() {
+   
    return (
       <>
          <div id='project-container'>
             <img id='tripit-img' src={tripIt} />
-            {/* <br/>
-
-            <a id='tripit-img-link' href="https://trip-it.onrender.com">
-            Trip It
-            </a> */}
          </div>
       </>
    )
 }
 
 export default TripItDiv;
+
+//To place elsewhere
+// /* <br/>
+
+//    <a id='tripit-img-link' href="https://trip-it.onrender.com">
+//    Trip It
+//    </a> */
