403c1b8255746757ec57bf95b5e7be1e4c44b06d
Charles Tyra <charles.tyra@gmail.com>
2024-04-03 22:12:55 -0400

    Note for ProjectDescriptionDiv

--- src/components/InteractiveDiv/ProjectsDiv/ProjectDescriptionDiv/index.js
+++ src/components/InteractiveDiv/ProjectsDiv/ProjectDescriptionDiv/index.js
@@ -3,8 +3,10 @@ import './ProjectDescriptionDiv.css';
 
 function ProjectDescriptionDiv({projectState}) {
 
+   //Change formatting of the words - have descriptions that match the vibes + links.
+
    return (
-      <div>
+      <div id="project-container">
          {projectState === 'TRIPIT'
             ?  <div>
                   MERN app where friends can collaborate to create, plan, 
@@ -18,11 +20,6 @@ function ProjectDescriptionDiv({projectState}) {
                   Fullstack Amazon clone where people can create a user account,
                   search and review products, add them to cart and purchase them
                </div>
-            : null 
-         }
-
-         {projectState === 'TRIPIT'
-            ?  <div>test statement</div>
             : null 
          }
       </div>
