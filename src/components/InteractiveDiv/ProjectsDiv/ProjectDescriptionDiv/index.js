21e75d97839730f3bbb675fd6080a1c5cb0aed5f
Charles Tyra <charles.tyra@gmail.com>
2024-04-13 23:27:45 -0400

    Project Description base css

--- src/components/InteractiveDiv/ProjectsDiv/ProjectDescriptionDiv/index.js
+++ src/components/InteractiveDiv/ProjectsDiv/ProjectDescriptionDiv/index.js
@@ -8,15 +8,14 @@ function ProjectDescriptionDiv({projectState}) {
    return (
       <div id="project-container">
          {projectState === 'TRIPIT'
-            ?  <div>
-                  MERN app where friends can collaborate to create, plan, 
-                  and make decisions about group travel
+            ?  <div id="project-description-div">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque viverra sapien ac euismod hendrerit. Donec vitae nisi id nunc convallis efficitur et eu lorem. Nunc vehicula maximus est eget laoreet. Quisque id libero et nibh ornare feugiat. Integer sit amet velit vitae risus facilisis semper in tempus turpis. Aenean sollicitudin interdum elementum. Etiam interdum congue augue, sed dignissim purus mattis vel. Suspendisse est lacus, imperdiet at diam ut, egestas congue nibh. Proin pellentesque a nulla vel iaculis. Aenean sit amet quam erat. Nulla viverra finibus ante, sit amet mollis quam semper ac. Mauris non nisl laoreet, sodales est sed, finibus ipsum.
                </div>
             : null 
          }
 
          {projectState === 'SAHARA'
-            ?  <div>
+            ?  <div id="project-description-div">
                   Fullstack Amazon clone where people can create a user account,
                   search and review products, add them to cart and purchase them
                </div>
