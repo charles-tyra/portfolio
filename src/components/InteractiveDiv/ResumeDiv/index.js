82e67534ff737361a45fbb0fe90a78079cb0a673
Charles Tyra <charles.tyra@gmail.com>
2024-04-20 21:10:08 -0400

    Notes on resume visuals.

--- src/components/InteractiveDiv/ResumeDiv/index.js
+++ src/components/InteractiveDiv/ResumeDiv/index.js
@@ -7,6 +7,8 @@ import testresume from '../../../assets/testresume.pdf';
 import 'react-pdf/dist/Page/AnnotationLayer.css';
 import './ResumeDiv.css';
 
+// Think about general use of switches from background to resume - whether it makes sense
+// to pull someone's vision so harshly.
 function ResumeDiv() {
    let [resumeWidth, setResumeWidth] = useState(window.innerWidth * 18 / 64);
 
