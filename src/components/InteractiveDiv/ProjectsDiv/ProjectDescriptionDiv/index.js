1dd17a30e7e61c10992a9db7c8a5a72f9e96ba8a
Charles Tyra <charles.tyra@gmail.com>
2024-04-27 19:10:51 -0400

    Base Sahara description CSS

--- src/components/InteractiveDiv/ProjectsDiv/ProjectDescriptionDiv/index.js
+++ src/components/InteractiveDiv/ProjectsDiv/ProjectDescriptionDiv/index.js
@@ -9,6 +9,7 @@ function ProjectDescriptionDiv({projectState}) {
       <div id="project-container">
          {projectState === 'TRIPIT'
             ?  <div id="project-description-div">
+                  
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque viverra sapien ac euismod hendrerit. Donec vitae nisi id nunc convallis efficitur et eu lorem. Nunc vehicula maximus est eget laoreet. Quisque id libero et nibh ornare feugiat. Integer sit amet velit vitae risus facilisis semper in tempus turpis. Aenean sollicitudin interdum elementum. Etiam interdum congue augue, sed dignissim purus mattis vel. Suspendisse est lacus, imperdiet at diam ut, egestas congue nibh. Proin pellentesque a nulla vel iaculis. Aenean sit amet quam erat. Nulla viverra finibus ante, sit amet mollis quam semper ac. Mauris non nisl laoreet, sodales est sed, finibus ipsum.
                </div>
             : null 
@@ -16,6 +17,15 @@ function ProjectDescriptionDiv({projectState}) {
 
          {projectState === 'SAHARA'
             ?  <div id="project-description-div">
+                  <div id='project-description-top-row'>
+                     <div id='project-description-links'>
+                        <a href='https://sahara-nq7b.onrender.com'>live</a>
+                        <div className="vertical-line"/>
+                        <a href='https://github.com/charles-tyra/sahara'>github repo</a>
+                     </div>
+                     <div id='sahara-line'/>
+                  </div>
+
                   Fullstack Amazon clone where people can create a user account,
                   search and review products, add them to cart and purchase them
                </div>
