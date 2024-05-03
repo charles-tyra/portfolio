4e6a315cf8d101da517d771f2897ada90e27c826
Charles Tyra <charles.tyra@gmail.com>
2024-05-03 13:25:05 -0400

    TripItDescription movement

--- src/components/InteractiveDiv/ProjectsDiv/ProjectDescriptionDiv/TripItDescriptionDiv/index.js
+++ src/components/InteractiveDiv/ProjectsDiv/ProjectDescriptionDiv/TripItDescriptionDiv/index.js
@@ -4,7 +4,10 @@ import './TripItDescriptionDiv.css';
 function TripItDescriptionDiv() {
 
    return (
-      <div />
+      <div id="project-description-div">
+
+         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque viverra sapien ac euismod hendrerit. Donec vitae nisi id nunc convallis efficitur et eu lorem. Nunc vehicula maximus est eget laoreet. Quisque id libero et nibh ornare feugiat. Integer sit amet velit vitae risus facilisis semper in tempus turpis. Aenean sollicitudin interdum elementum. Etiam interdum congue augue, sed dignissim purus mattis vel. Suspendisse est lacus, imperdiet at diam ut, egestas congue nibh. Proin pellentesque a nulla vel iaculis. Aenean sit amet quam erat. Nulla viverra finibus ante, sit amet mollis quam semper ac. Mauris non nisl laoreet, sodales est sed, finibus ipsum.
+      </div>
    )
 }
 
