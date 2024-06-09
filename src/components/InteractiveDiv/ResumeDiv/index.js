62df3f1691261d94bf1f993c3577b3d8624b7205
Charles Tyra <charles.tyra@gmail.com>
2024-06-08 23:13:40 -0400

    Unused import code.

--- src/components/InteractiveDiv/ResumeDiv/index.js
+++ src/components/InteractiveDiv/ResumeDiv/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { Document, Page, pdfjs} from 'react-pdf';
 import { PDFDownloadLink } from "@react-pdf/renderer";
 
