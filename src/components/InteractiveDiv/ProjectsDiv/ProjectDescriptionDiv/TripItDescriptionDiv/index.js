3f22ee3d38412e5584479a6fe3fff150d4a17e62
Charles Tyra <charles.tyra@gmail.com>
2024-07-13 22:47:46 -0400

    More small visual changes.

--- src/components/InteractiveDiv/ProjectsDiv/ProjectDescriptionDiv/TripItDescriptionDiv/index.js
+++ src/components/InteractiveDiv/ProjectsDiv/ProjectDescriptionDiv/TripItDescriptionDiv/index.js
@@ -14,8 +14,8 @@ function TripItDescriptionDiv() {
          </div>
          Designed a bespoke “travel-themed” user interface (UI) playing on the idea of postcards for a collaborative trip planning web application including manually drafting assets to ensure consistent aesthetics and spacing when rendered on the frontend.
          <br/>
-         Adopted an agile workflow for a highly optimized development sprint including daily standups to identify any challenges or blockers along with git for version control to meet a 10-day deadline.
          <br/>
+         &emsp; Adopted an agile workflow for a highly optimized development sprint including daily standups to identify any challenges or blockers along with git for version control to meet a 10-day deadline.
          Leveraged strong coding knowledge and extensive detail work to navigate the team codebase conducting review, troubleshooting, and debugging for streamlining project sprint. 
       </div>
    )
