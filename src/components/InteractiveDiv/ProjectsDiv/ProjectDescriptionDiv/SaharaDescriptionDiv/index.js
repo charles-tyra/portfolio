46b580e2142183376e87ec0469bc35b2210ea65a
Charles Tyra <charles.tyra@gmail.com>
2024-07-13 22:41:40 -0400

    Spacing visualization Sahara

--- src/components/InteractiveDiv/ProjectsDiv/ProjectDescriptionDiv/SaharaDescriptionDiv/index.js
+++ src/components/InteractiveDiv/ProjectsDiv/ProjectDescriptionDiv/SaharaDescriptionDiv/index.js
@@ -12,7 +12,8 @@ function SaharaDescriptionDiv() {
             </div>
             <div id='sahara-line' />
          </div>
-         &emsp; Crafted a truly pixel-perfect Amazon clone including reverse engineering particular digital assets from the proprietary codebase to ensure a heavily accurate, detailed recreation of the early 2023 website.
+         Crafted a truly pixel-perfect Amazon clone including reverse engineering particular digital assets from the proprietary codebase to ensure a heavily accurate, detailed recreation of the early 2023 website.
+         <br/>
          <br/>
          &emsp; Customized CSS selectors for dynamic and resizable website visuals to adhere to accessibility and user experience best practices.
          Utilized a Ruby on Rails backend paired with a React.js frontend in order to build a fully deployed full-stack website with AWS(S3) and PostgresQL bucket storage for full CRUD functionality across the app.
